07599c87c4a679b9cb292f6b68c4e3ba899d4db5
Julius <njmwas@gmail.com>
2026-07-07 10:35:17 +0300

    Dynamic elements with js

--- src/main.js
+++ src/main.js
@@ -14,10 +14,28 @@ document
 // Difference between querySelector and querySelectorAll
 // Node
 const header = document.createElement("header");
-header.innerText = "Moringa Social";
+
+const h3 = document.createElement("h3")
+h3.innerText = "Moringa Social"
+
+const a = document.createElement("a")
+a.innerText = "Sign In"
+
+const a1 = document.createElement("a")
+a1.innerText = "Register";
+
+header.append(h3, a, a1);
+
+const main = document.createElement("main")
+main.innerHTML = `<div><h6>Main</h6></div>`
 
 const app = document.querySelector("#app");
 app.appendChild(header);
+app.appendChild(main);
+
+const aside = document.createElement("aside")
+aside.innerText = "Aside"
+app.insertBefore(aside, main)
 
 
 console.log(header);
