b62e471655be755e3aaa5baa43a426fa115af155
Julius <njmwas@gmail.com>
2026-07-07 09:53:30 +0300

    DOM selectors

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+
+/**
+ * When encounter a js file
+ * Take all the html elements and create a 
+ * js object that references all the current html elements
+ * called the DOM(Document Object Model)
+ */
+
+window
