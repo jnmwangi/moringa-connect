f06236b139eef6e5014045a69cab6968d808a411
Julius <njmwas@gmail.com>
2026-07-08 14:36:17 +0300

    Render feeds dynamically and like click events

--- src/main.js
+++ src/main.js
@@ -1,62 +1,20 @@
 import './style.css'
+import { feeds } from "./data";
+import { feedUIComponent } from './feed';
 
+const main = document.querySelector("main");
 
-/**
- * When encounter a js file
- * Take all the html elements and create a 
- * js object that references all the current html elements
- * called the DOM(Document Object Model)
- */
+// How do we render our feeds dynamically using the data and 
+// feedUIComponent function. Iterate through data
 
-// if document is an object, what does it give us as devs
-document
+main.innerHTML = feeds.map(feedUIComponent).join("");
 
-// Difference between querySelector and querySelectorAll
-// Node
+document.querySelectorAll(".like-button").forEach(button => {
+    button.addEventListener("click", (evnt) => {
+        const feedId = Number(button.getAttribute("data-feedid"));
+        const feed = feeds.find(f => f.id === feedId)
+        feed.likes += 1;
 
-function elemetor(tagname, ...children){
-    const element = document.createElement(tagname);
-    element.append(...children);
-    return element;
-}
-
-const header = elemetor("header", 
-    elemetor("h3", "Moringa Social"),
-    elemetor("ul", 
-        elemetor("li", "Sign In"),
-        elemetor("li", "Registration")
-    )
-)
-
-const main = elemetor("main", 
-    elemetor("div", "I am main")
-)
-
-app.append(header, main)
-
-/* const header = document.createElement("header");
-
-const h3 = document.createElement("h3")
-h3.innerText = "Moringa Social"
-
-const a = document.createElement("a")
-a.innerText = "Sign In"
-
-const a1 = document.createElement("a")
-a1.innerText = "Register";
-
-header.append(h3, a, a1);
-
-const main = document.createElement("main")
-main.innerHTML = `<div><h6>Main</h6></div>`
-
-const app = document.querySelector("#app");
-app.appendChild(header);
-app.appendChild(main);
-
-const aside = document.createElement("aside")
-aside.innerText = "Aside"
-app.insertBefore(aside, main)
-
-
-console.log(header); */
+        button.querySelector(".num-likes").textContent = feed.likes;
+    });
+});
